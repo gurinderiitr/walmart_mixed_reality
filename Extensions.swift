3f0504a8eac6adea9e24c1fe268057f1fa699a20
Pedro Cardoso <pedro@grok.pt>
2019-01-24 16:21:46 +0000

    WIP

---
+++ Extensions.swift
@@ -0,0 +1,127 @@
+//
+//  Extensions.swift
+//  Mixed Reality
+//
+//  Created by Pedro Cardoso on 24/01/2019.
+//  Copyright © 2019 Mokriya LLC. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+import VideoToolbox
+import AVFoundation
+
+extension CGRect {
+    func scaled(by factor: CGFloat) -> CGRect {
+        return CGRect(x: minX * factor, y: minY * factor, width: width * factor, height: height * factor)
+    }
+    
+    var aspectRatio: CGFloat {
+        return width / height
+    }
+}
+
+extension UIImage {
+    
+    func fixedOrientation() -> UIImage? {
+        
+        guard imageOrientation != UIImage.Orientation.up else {
+            //This is default orientation, don't need to do anything
+            return self.copy() as? UIImage
+        }
+        
+        guard let cgImage = self.cgImage else {
+            //CGImage is not available
+            return nil
+        }
+        
+        guard let colorSpace = cgImage.colorSpace, let ctx = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: 0, space: colorSpace, bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue) else {
+            return nil //Not able to create CGContext
+        }
+        
+        var transform: CGAffineTransform = CGAffineTransform.identity
+        
+        switch imageOrientation {
+        case .down, .downMirrored:
+            transform = transform.translatedBy(x: size.width, y: size.height)
+            transform = transform.rotated(by: CGFloat.pi)
+            break
+        case .left, .leftMirrored:
+            transform = transform.translatedBy(x: size.width, y: 0)
+            transform = transform.rotated(by: CGFloat.pi / 2.0)
+            break
+        case .right, .rightMirrored:
+            transform = transform.translatedBy(x: 0, y: size.height)
+            transform = transform.rotated(by: CGFloat.pi / -2.0)
+            break
+        case .up, .upMirrored:
+            break
+        }
+        
+        //Flip image one more time if needed to, this is to prevent flipped image
+        switch imageOrientation {
+        case .upMirrored, .downMirrored:
+            transform.translatedBy(x: size.width, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+            break
+        case .leftMirrored, .rightMirrored:
+            transform.translatedBy(x: size.height, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+        case .up, .down, .left, .right:
+            break
+        }
+        
+        ctx.concatenate(transform)
+        
+        switch imageOrientation {
+        case .left, .leftMirrored, .right, .rightMirrored:
+            ctx.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: size.height, height: size.width))
+        default:
+            ctx.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            break
+        }
+        
+        guard let newCGImage = ctx.makeImage() else { return nil }
+        return UIImage.init(cgImage: newCGImage, scale: 1, orientation: .up)
+    }
+}
+extension UIImage {
+    
+    public convenience init?(pixelBuffer: CVPixelBuffer) {
+        var cgImage: CGImage?
+        VTCreateCGImageFromCVPixelBuffer(pixelBuffer, options: nil, imageOut: &cgImage)
+        guard let c = cgImage else { return nil }
+        self.init(cgImage: c, scale: 1, orientation: UIImage.Orientation.right)
+    }
+    
+    func cropped(boundingBox: CGRect) -> UIImage? {
+        guard let cgImage = self.cgImage?.cropping(to: boundingBox) else {
+            return nil
+        }
+        return UIImage(cgImage: cgImage)
+    }
+    
+    var withGrayscale: UIImage {
+        guard let ciImage = CIImage(image: self, options: nil) else { return self }
+        let paramsColor: [String: AnyObject] = [kCIInputBrightnessKey: NSNumber(value: 0.0), kCIInputContrastKey: NSNumber(value: 1.0), kCIInputSaturationKey: NSNumber(value: 0.0)]
+        let grayscale = ciImage.applyingFilter("CIColorControls", parameters: paramsColor)
+        guard let processedCGImage = CIContext().createCGImage(grayscale, from: grayscale.extent) else { return self }
+        return UIImage(cgImage: processedCGImage, scale: scale, orientation: imageOrientation)
+    }
+    
+}
+
+extension matrix_float4x4 {
+    func position(offset: SCNVector3 = SCNVector3Zero) -> SCNVector3 {
+        let mat = SCNMatrix4(self)
+        return SCNVector3(mat.m41 + offset.x, mat.m42 + offset.y, mat.m43 + offset.z)
+    }
+}
+
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
+}
--- Mixed Reality/ViewController.swift
+++ Mixed Reality/ViewController.swift
@@ -9,43 +9,17 @@
 import UIKit
 import SceneKit
 import ARKit
+import VideoToolbox
+import AVFoundation
 
-extension UIImage {
-    
-    func cropped(boundingBox: CGRect) -> UIImage? {
-        guard let cgImage = self.cgImage?.cropping(to: boundingBox) else {
-            return nil
-        }
-        return UIImage(cgImage: cgImage)
-    }
-
-    var withGrayscale: UIImage {
-        guard let ciImage = CIImage(image: self, options: nil) else { return self }
-        let paramsColor: [String: AnyObject] = [kCIInputBrightnessKey: NSNumber(value: 0.0), kCIInputContrastKey: NSNumber(value: 1.0), kCIInputSaturationKey: NSNumber(value: 0.0)]
-        let grayscale = ciImage.applyingFilter("CIColorControls", parameters: paramsColor)
-        guard let processedCGImage = CIContext().createCGImage(grayscale, from: grayscale.extent) else { return self }
-        return UIImage(cgImage: processedCGImage, scale: scale, orientation: imageOrientation)
-    }
-}
-
-extension matrix_float4x4 {
-    func position() -> SCNVector3 {
-        let mat = SCNMatrix4(self)
-        return SCNVector3(mat.m41, mat.m42, mat.m43)
-    }
-}
-
-extension float4x4 {
-    var translation: float3 {
-        let translation = self.columns.3
-        return float3(translation.x, translation.y, translation.z)
-    }
-}
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var planeNodes: [SCNNode] = []
+    var addPlane = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -112,6 +86,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        guard addPlane else { return }
+        
         // 1
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
 
@@ -121,7 +98,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let plane = SCNPlane(width: width, height: height)
 
         // 3
-        plane.materials.first?.diffuse.contents = UIColor.blue.withAlphaComponent(0.2)
+        plane.materials.first?.diffuse.contents = UIColor.blue.withAlphaComponent(0.3)
 
         // 4
         let planeNode = SCNNode(geometry: plane)
@@ -135,6 +112,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // 6
         node.addChildNode(planeNode)
+        
+        planeNodes.append(planeNode)
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
@@ -164,62 +143,70 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         print("plane distance: \(res.distance)")
         
+        guard let pic = UIImage(pixelBuffer: sceneView.session.currentFrame!.capturedImage)?.fixedOrientation() else { return }
+        
+        let insideRect = AVMakeRect(aspectRatio: sceneView.bounds.size, insideRect: CGRect(origin: .zero, size: pic.size))
+        let pic2 = pic.cropped(boundingBox: insideRect)
+        
+        let size = res.distance / 2
        
-        let planeGeometry = SCNPlane(width: 0.1, height: 0.1)
-        planeGeometry.firstMaterial?.diffuse.contents = UIColor.red.withAlphaComponent(0.2)
+        let planeGeometry = SCNPlane(width: size, height: size)
+        planeGeometry.firstMaterial?.diffuse.contents = UIColor.red.withAlphaComponent(0.9)
         let planeNode = SCNNode(geometry: planeGeometry)
 
         planeNode.simdTransform = anchor.transform
         planeNode.eulerAngles = SCNVector3Make(planeNode.eulerAngles.x - (Float.pi / 2), planeNode.eulerAngles.y, planeNode.eulerAngles.z)
-        planeNode.position = res.worldTransform.position()
+        planeNode.position = res.worldTransform.position(offset: SCNVector3(0, 0, 0.01))
         
       
         sceneView.scene.rootNode.addChildNode(planeNode)
-    
-//        guard let shipScene = SCNScene(named: "ship.scn"),
-//            let shipNode = shipScene.rootNode.childNode(withName: "ship", recursively: false)
-//            else { return }
-//
-//
-//        shipNode.position = SCNVector3(x,y,z)
-//        sceneView.scene.rootNode.addChildNode(shipNode)
         
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) { [weak planeNode, weak sceneView] in
+        //        guard let shipScene = SCNScene(named: "ship.scn"),
+        //            let shipNode = shipScene.rootNode.childNode(withName: "ship", recursively: false)
+        //            else { return }
+        //
+        //
+        //        shipNode.position = SCNVector3(x,y,z)
+        //        sceneView.scene.rootNode.addChildNode(shipNode)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.2) { [weak planeNode, weak sceneView] in
             
             guard let planeNode = planeNode, let sceneView = sceneView else { return }
-        let bbox = planeNode.presentation.boundingBox
+            let bbox = planeNode.presentation.boundingBox
+            
+            // world coordinates
+            let v1w =  planeNode.convertPosition(bbox.min, to: sceneView.scene.rootNode)
+            let v2w =  planeNode.convertPosition(bbox.max, to: sceneView.scene.rootNode)
+            
+//            v1w.z = 0
+//            v2w.z = 0
+            
+            //projected coordinates
+            let v1p = sceneView.projectPoint(v1w)
+            let v2p = sceneView.projectPoint(v2w)
+            
+            //frame rectangle
+            let rect = CGRect(x: CGFloat(v1p.x), y: CGFloat(v2p.y), width: max(10, CGFloat(v2p.x - v1p.x)), height: max(10, CGFloat(v1p.y - v2p.y)))
+            
+//            let rectView = UIView(frame: rect)
+//            rectView.alpha = 0.3
+//            rectView.backgroundColor = UIColor.purple
+//            sceneView.addSubview(rectView)
+            
+            print("v1 \(bbox.min), v2\(bbox.max)")
+            print("v1w \(v1w), v2w \(v2w)")
+            print("v1p \(v1p), v2p \(v2p)")
+            print("rect\(rect)")
+            
+            let sel = pic2!.withGrayscale.cropped(boundingBox: rect.scaled(by: pic2!.size.width / sceneView.bounds.width))
+           
+            planeGeometry.firstMaterial?.diffuse.contents = sel
+        }
         
-        print("bbox: \(bbox)")
-        
-        // world coordinates
-        let v1w =  planeNode.convertPosition(bbox.min, to: sceneView.scene.rootNode)
-        let v2w =  planeNode.convertPosition(bbox.max, to: sceneView.scene.rootNode)
-        
-//        v1w.z = 0
-//        v2w.z = 0
-        
-        //projected coordinates
-        let v1p = sceneView.projectPoint(v1w)
-        let v2p = sceneView.projectPoint(v2w)
-        
-        //frame rectangle
-        let rect = CGRect(x: CGFloat(v1p.x), y: CGFloat(v2p.y), width: max(10, CGFloat(v2p.x - v1p.x)), height: max(10, CGFloat(v1p.y - v2p.y)))
-
-        let rectView = UIView(frame: rect)
-        rectView.alpha = 0.3
-        rectView.backgroundColor = UIColor.purple
-        sceneView.addSubview(rectView)
-    }
-
-//        let pic = sceneView.snapshot().withGrayscale
-//        let sel = pic.cropped(boundingBox: rect)
-//        planeGeometry.firstMaterial?.diffuse.contents = sel
-        
-//        print("v1 \(bbox.min), v2\(bbox.max)")
-//        print("v1w \(v1w), v2w \(v2w)")
-//        print("v1p \(v1p), v2p \(v2p)")
-//        print("rect\(rect)")
-        
+        addPlane = false
+        planeNodes.forEach {
+            $0.removeFromParentNode()
+        }
     }
     
     func updatePositionAndOrientationOf(_ node: SCNNode, withPosition position: SCNVector3, relativeTo referenceNode: SCNNode) {
